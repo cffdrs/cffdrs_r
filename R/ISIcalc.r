9aeea8d55bbc7538fdcc5428b891f7b789d7b14d
Alan Cantin <alan.cantin@canada.ca>
2016-03-14 17:58:02 +0000

    One more & than necessary

--- R/ISIcalc.r
+++ R/ISIcalc.r
@@ -19,6 +19,7 @@
   #   ffmc:   Fine Fuel Moisture Code
   #     ws:   Wind Speed (km/h)
   # fbpMod:   TRUE/FALSE if using the fbp modification at the extreme end
+  #
   # Returns:
   #   ISI:    Intial Spread Index
   #
@@ -28,12 +29,12 @@
   #Eq. 24 - Wind Effect
   #the ifelse, also takes care of the ISI modification for the fbp functions
   # This modification is Equation 53a in FCFDG (1992)
-  fW   <- ifelse(ws >= 40 && fbpMod==TRUE,
+  fW   <- ifelse(ws >= 40 & fbpMod==TRUE,
                  12 * (1 - exp(-0.0818 * (ws - 28))), 
                  exp(0.05039 * ws))
   #Eq. 25 - Fine Fuel Moisture
   fF <- 91.9 * exp(-0.1386 * fm) * (1 + (fm^5.31) / 49300000)
   #Eq. 26 - Spread Index Equation
   isi <- 0.208 * fW * fF
-  return(isi)  
+  return(isi)
 }
