7e4c785c58f21d57a4376a61953da60b0c601f1a
Alan Cantin <alan.cantin@canada.ca>
2017-03-16 13:35:07 +0000

    missing a /4 in the equation. fixed. Thanks Bo :)

--- R/ROSthetacalc.r
+++ R/ROSthetacalc.r
@@ -28,7 +28,7 @@
     ((ROS - BROS) / (2 * c1) + 
     (ROS + BROS)/(2*c1)) *
 	  ((FROS * c1 * sqrt(FROS * FROS * c1 * c1 + (ROS * BROS) * s1 * s1) - 
-	    (ROS * ROS - BROS * BROS) * s1 * s1) /
+	    ((ROS * ROS - BROS * BROS)/4) * s1 * s1) /
 		 (FROS * FROS * c1 * c1 + ((ROS + BROS) / 2) * ((ROS + BROS) / 2) * s1 * s1)
 	  )
 	 return(ROStheta)
