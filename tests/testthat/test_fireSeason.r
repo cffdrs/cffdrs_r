28dfee318f2ed5a8b2fae461c7684de9d409959d
Jordan Evens <jordan.evens@canada.ca>
2022-04-21 09:45:46 -0400

    restored fireSeason()

--- tests/testthat/test_fireSeason.r
+++ tests/testthat/test_fireSeason.r
@@ -1,7 +1,7 @@
 test_that("FireSeason_test_wDC_1", {
   test_wDC <- read.csv('../../data/test_wDC.csv', sep=';')
   input <- with(test_wDC, test_wDC[order(id,yr,mon,day),])
-  result <- FireSeason(input[input$id==1,])
+  result <- fireSeason(input[input$id==1,])
   # HACK: convert date to character so it matches how file is read
   result$date <- as.character(result$date)
   checkResults('FireSeason_test_wDC_1',
@@ -10,7 +10,7 @@ test_that("FireSeason_test_wDC_1", {
 test_that("FireSeason_test_wDC_2", {
   test_wDC <- read.csv('../../data/test_wDC.csv', sep=';')
   input <- with(test_wDC, test_wDC[order(id,yr,mon,day),])
-  result <- FireSeason(input[input$id==1,],fs.start=10, fs.end=3)
+  result <- fireSeason(input[input$id==1,],fs.start=10, fs.end=3)
   # HACK: convert date to character so it matches how file is read
   result$date <- as.character(result$date)
   checkResults('FireSeason_test_wDC_2',
@@ -19,7 +19,7 @@ test_that("FireSeason_test_wDC_2", {
 test_that("FireSeason_test_wDC_3", {
   test_wDC <- read.csv('../../data/test_wDC.csv', sep=';')
   input <- with(test_wDC, test_wDC[order(id,yr,mon,day),])
-  result <- FireSeason(input[input$id==2,],method="WF93")
+  result <- fireSeason(input[input$id==2,],method="WF93")
   # HACK: convert date to character so it matches how file is read
   result$date <- as.character(result$date)
   checkResults('FireSeason_test_wDC_3',
