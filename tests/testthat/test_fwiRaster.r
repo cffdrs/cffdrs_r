d714e6b12681412f4a92397a5e77f94d191f16b9
Jordan Evens <jordan.evens@nrcan-rncan.gc.ca>
2024-01-11 15:13:54 -0500

    fixed fwi test checking wrong condition and implement the other 2 tests

--- tests/testthat/test_fwiRaster.r
+++ tests/testthat/test_fwiRaster.r
@@ -1,31 +1,51 @@
-test_that("Output length is input length", {
-
-  ## Equal input / output length.
-  test_fwi <- rast("tests/data/rasters/fwiRaster_test1/fwiRaster_test1.tif")
-  names(test_fwi) <- c("TEMP", "RH", "WS", "PREC", "FFMC", "DMC", "DC", "ISI", "BUI", "FWI", "DSR")
+test_raster <- function(name, fct) {
   day01src <- rast(
     system.file("extdata", "test_rast_day01.tif", package = "cffdrs")
   )
   names(day01src) <- c("temp", "rh", "ws", "prec")
   day01 <- crop(day01src, c(250, 255, 47, 51))
+
+  test_fwi <- rast(sprintf("tests/data/rasters/%s/%s.tif", name, name))
   test_fwi <- crop(test_fwi, c(250, 255, 47, 51))
-  # assign variable names:
-  day01_out <- fwiRaster(input = day01, out = "all")
+
+  day01_out <- fct(day01)
   out_cols <- setdiff(names(day01_out), toupper(names(day01)))
-  #day01 <- subset(x = day01, subset = "LAT", negate = T)
+  # we don't actually know the names of the columns from the file, so assign from output
+  # names(test_fwi) <- c("TEMP", "RH", "WS", "PREC", "FFMC", "DMC", "DC", "ISI", "BUI", "FWI", "DSR")
+  names(test_fwi) <- names(day01_out)
+
   m <- minmax(day01_out[[out_cols]] - test_fwi[[out_cols]])
   expect_true(all(abs(m) < 1e-5))
 
   day01_dt <- data.frame(day01_out)
   fwi_dt <- data.frame(test_fwi)
 
-  expect_true(all(abs(day01_dt - fwi_dt) > 1e-5))
+  expect_true(all(abs(day01_dt - fwi_dt) < 1e-5))
+}
+
+test_that("fwiRaster_test1", {
+  test_raster(
+    "fwiRaster_test1",
+    function(input) {
+      fwiRaster(input = input)
+    }
+  )
 })
 
-# test_that("The function produces the expected output.", {
-#   ## Equal Output
-#   test_fwiRaster_out <- rast(system.file("./inst/extdata/test_fwiRaster_out.tif", package = "cffdrs"))
-#   test_fwiRaster_out <- mask(test_fwiRaster_out,day01)
-#   expect_equal(object = round(fwiRaster(input = day01)[[1]][],2),expected = round(test_fwiRaster_out[[1]][],2))
-#
-# })
+test_that("fwiRaster_test2", {
+  test_raster(
+    "fwiRaster_test2",
+    function(input) {
+      fwiRaster(input = input, out = "all")
+    }
+  )
+})
+
+test_that("fwiRaster_test3", {
+  test_raster(
+    "fwiRaster_test3",
+    function(input) {
+      fwiRaster(input = input, out = "fwi")
+    }
+  )
+})
