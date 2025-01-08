90ec88f3d8bc05f9ff484b5ddf3637b0c97caa3f
Brett M <brett.moore@nrcan-rncan.gc.ca>
2025-01-08 08:52:05 -0700

    FMC update

--- R/foliar_moisture_content.r
+++ R/foliar_moisture_content.r
@@ -9,15 +9,22 @@
 #' @param LAT    Latitude (decimal degrees)
 #' @param LONG   Longitude (decimal degrees)
 #' @param ELV    Elevation (metres)
-#' @param DJ     Day of year (offeren referred to as julian date)
+#' @param DJ     Day of year (julian date)
 #' @param D0     Date of minimum foliar moisture content. _If D0, date of min
 #'                   FMC, is not known then D0 = NULL._
+#' @param FMCo Foliar Moisture Content Override - For use when outside of North 
+#'                America
 #'
 #' @return FMC: Foliar Moisture Content value
 #' @noRd
 
-foliar_moisture_content <- function(LAT, LONG, ELV, DJ, D0) {
+foliar_moisture_content <- function(LAT, LONG, ELV, DJ, D0,FMCo=NULL) {
 
+  if(!is.null(FMCo) & LAT < 0){FMC <- FMCo;
+                     message("FMC Override provided, returning as FMC.");
+                     return(FMC)
+            }
+  warning({if(LAT < 7 | LONG < 140 | LONG > 52){"Location outside of North America. Please define an FMC override in the FMCo variable."}})
   D0 <- foliar_moisture_content_minimum(LAT, LONG, ELV, DJ, D0)
 
   # Number of days between day of year and date of min FMC
